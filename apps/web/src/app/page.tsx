0c44461b490e17120845508c046e55a224da35e3
WonJo-Jung <unprecedentedgame222@gmail.com>
2025-09-21 17:26:51 +0900

    fresh apps/web/src

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,11 +1,7 @@
 import { ConnectButton } from "@ricepay/web-lib";
 import SendUSDCForm from "../components/SendUSDCForm";
 import FxCard from "../components/FxCard";
-import { fxTtlSeconds } from "../lib/config";
 import { fetchUsdMxnServer } from "../lib/fx-server";
-
-// 이 파일에서 수행하는 fetch의 기본 ISR 주기를 .env와 동기화
-export const revalidate = fxTtlSeconds;
 
 export default async function Page() {
   const data = await fetchUsdMxnServer(); // 초기 데이터
