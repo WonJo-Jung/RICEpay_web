d20e3c8750bbc81abb3b003f2d6812985b8ea7ee
WonJo-Jung <unprecedentedgame222@gmail.com>
2025-12-15 20:21:17 +0900

    fix  netlify.toml

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+
   transpilePackages: ['@reown/appkit', '@reown/appkit-utils', '@reown/appkit-adapter-wagmi'],
   webpack: (config) => {
     config.resolve.alias = {
